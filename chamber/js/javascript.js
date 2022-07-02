4109b049425c4f27c63057b06e8f7ed842b51c34
zrobker <zrobker@byui.edu>
2022-07-02 18:34:19 -0400

    directory

--- chamber/js/javascript.js
+++ chamber/js/javascript.js
@@ -38,3 +38,18 @@ function get_day_month_year() {
     datefield.innerHTML = `<em>${fulldate}</em>`;
 }
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+listbutton.addEventListener("click", showList);
+gridbutton.addEventListener("click", showGrid);
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+function showGrid() {
+	display.classList.add("grid");
+	display.classList.remove("list");
+}
