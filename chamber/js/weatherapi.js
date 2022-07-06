39cf3289ec507fa16940cf77ffb98aed0a22c2bf
zrobker <zrobker@byui.edu>
2022-07-06 09:50:03 -0400

    weatherapi

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector('.current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const currentWind = document.querySelector('.current-wind');
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Hartsville&units=Imperial&appid=e4d71b43d1246bd2ccf442ae7d37e916`;
+
+apiFetch(url);
+
+async function apiFetch(url) {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+}
+  
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
+    currentWind.innerHTML = `<strong>${weatherData.wind.speed.toFixed(1)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
